0100823a2bd8e71ac642923e6d6368e931791454
riteemo <irina.qqu@gmail.com>
2024-10-25 23:51:19 +0300

    pep

--- RegexpFinder.py
+++ RegexpFinder.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from regexp import find_tandem_words_in_text
 from requests import get
 from requests.exceptions import MissingSchema
@@ -8,17 +10,17 @@ class RegexpFinder:
     def __init__(self, regexp: str):
         self.regexp = regexp
 
-    def find_in_string(self, input_txt: str):
+    def find_in_string(self, input_txt: str) -> list[str]:
         return find_tandem_words_in_text(input_txt, self.regexp)
 
-    def find_on_page(self, url: str):
+    def find_on_page(self, url: str) -> list[str] | str:
         try:
             res = get(url).text
             return find_tandem_words_in_text(res, self.regexp)
         except MissingSchema:
             return "Wrong URL"
 
-    def find_in_file(self, filename: str):
+    def find_in_file(self, filename: str) -> list[str] | str:
         try:
             with open(filename) as file:
                 text = file.read()
